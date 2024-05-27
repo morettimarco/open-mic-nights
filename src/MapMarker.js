6482ce564670fe3255bcaa37f37b322345967a8e
morettimarco <marco.moretti@gmail.com>
2024-05-27 17:36:15 +0200

    Close info modal

--- src/MapMarker.js
+++ src/MapMarker.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { PiMicrophoneStageFill } from "react-icons/pi";
 
+// MapMarker Component
 class MapMarker extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { show: props.show };
   }
+
+  handleClose = () => {
+    this.setState({ show: false });
+  };
 
   render() {
     const markerStyle = {
@@ -18,13 +24,15 @@ class MapMarker extends React.Component {
         <PiMicrophoneStageFill
           style={markerStyle}
           className={this.props.status === "Active" ? "is-link" : "is-danger"}
+          onClick={() => this.setState({ show: !this.state.show })}
         />
-        {this.props.show && (
+        {this.state.show && (
           <InfoWindow
             name={this.props.name}
             address={this.props.address}
             weekday={this.props.weekday}
             status={this.props.status}
+            onClose={this.handleClose}
           />
         )}
       </div>
@@ -33,7 +41,7 @@ class MapMarker extends React.Component {
 }
 
 const InfoWindow = (props) => {
-  const { name, address, weekday } = props;
+  const { name, address, weekday, onClose } = props;
   const infoWindowStyle = {
     position: "relative",
     bottom: 50,
@@ -50,6 +58,7 @@ const InfoWindow = (props) => {
 
   return (
     <div style={infoWindowStyle}>
+      <button onClick={onClose} style={{ float: "right", background: "none", border: "none", cursor: "pointer" }}>✖️</button>
       <div style={{ fontSize: 14 }}>{name} </div>
       <div style={{ fontSize: 10 }}>
         {weekday} at {address}
